55459d48d725683b78d9da69621759878fb9c78b
Jose R Garcia <jotargarcia@live.com>
2020-12-07 23:53:04 -0400

    Removing Dhrystone brought from picoev32

--- ORC_R32I/dhrystone/stdlib.d
+++
@@ -1,9 +0,0 @@
-stdlib.o: stdlib.c \
- /home/jota/riscv/_install/lib/gcc/riscv32-unknown-elf/10.2.0/include/stdarg.h \
- /home/jota/riscv/_install/lib/gcc/riscv32-unknown-elf/10.2.0/include/stdint.h \
- /home/jota/riscv/_install/riscv32-unknown-elf/include/stdint.h \
- /home/jota/riscv/_install/riscv32-unknown-elf/include/machine/_default_types.h \
- /home/jota/riscv/_install/riscv32-unknown-elf/include/sys/features.h \
- /home/jota/riscv/_install/riscv32-unknown-elf/include/_newlib_version.h \
- /home/jota/riscv/_install/riscv32-unknown-elf/include/sys/_intsup.h \
- /home/jota/riscv/_install/riscv32-unknown-elf/include/sys/_stdint.h
