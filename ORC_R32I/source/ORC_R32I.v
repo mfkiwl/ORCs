7647764bed01f3f3d219ea2a45a1495555ca996b
Jose R Garcia <jotargarcia@live.com>
2020-12-12 00:16:25 -0400

    Rolling back part of the FSM changes that broke the WB interface

--- ORC_R32I/source/ORC_R32I.v
+++ ORC_R32I/source/ORC_R32I.v
@@ -33,7 +33,7 @@
 // File name     : ORC_R32I.v
 // Author        : Jose R Garcia
 // Created       : 2020/11/04 23:20:43
-// Last modified : 2020/12/11 11:06:41
+// Last modified : 2020/12/12 00:13:59
 // Project Name  : ORCs
 // Module Name   : ORC_R32I
 // Description   : The ORC_R32I is a machine mode capable hart implementation of 
@@ -245,6 +245,10 @@ module ORC_R32I #(
               r_program_counter_valid <= 1'b1;
               r_program_counter_state <= S_WAIT_FOR_ACK;
             end
+          end 
+          else begin
+            r_program_counter_valid <= 1'b0;
+            r_program_counter_state <= S_WAIT_FOR_ACK;
           end
           // Update the program counter.
           r_next_pc_decode <= r_next_pc_fetch;
